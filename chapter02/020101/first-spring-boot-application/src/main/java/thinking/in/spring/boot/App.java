4cff0f511ccc94d65c08bb309c521a3fc431955e
CoderDream <coderdream@gmail.com>
2021-06-10 11:06:30 +0800

    完成 2.1.1 命令行方式创建 Spring Boot 应用

--- chapter02/020101/first-spring-boot-application/src/main/java/thinking/in/spring/boot/App.java
+++ chapter02/020101/first-spring-boot-application/src/main/java/thinking/in/spring/boot/App.java
@@ -1,13 +1,24 @@
 package thinking.in.spring.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@RestController
+@SpringBootApplication
+public class App {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome, my Buddy!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        //System.out.println("Hello World!");
     }
 }
