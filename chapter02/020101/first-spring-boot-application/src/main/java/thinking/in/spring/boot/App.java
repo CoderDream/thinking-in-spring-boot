53f0c76fcebbd6d2364faa378b7f86591174b928
CoderDream <coderdream@gmail.com>
2021-06-10 10:20:13 +0800

    init first-spring-boot-application

---
+++ chapter02/020101/first-spring-boot-application/src/main/java/thinking/in/spring/boot/App.java
@@ -0,0 +1,13 @@
+package thinking.in.spring.boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
